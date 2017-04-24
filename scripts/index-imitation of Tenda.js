ff41273493a87d0f60f53a0696f2d28db26d2658
MT <mt001mt@163.com>
2017-04-24 23:00:29 +0800

    着手hover“家用产品”后显示div

--- scripts/index-imitation of Tenda.js
+++ scripts/index-imitation of Tenda.js
@@ -4,11 +4,19 @@ $(
   function () {
     //关闭下拉菜单功能
     $(document).off(".dropdown");
+    //关闭transition效果
+    //$.support.transition = false;
     //按需求重新添加下拉菜单功能
     $(".dropdown-toggle").dropdown();
     $(document).on("click", ".user-dropdown-toggle", function () {
       $(this).parent().toggleClass("open");
       return false;
+    });
+    //添加hover事件，触发后调用collapse()函数
+    $(document).on("click", ".user-collapse-toggle", function (e) {
+      //$.support.transition = true;
+      $($(e.target).attr("data-target")).collapse();
+      //$(document).
     });
   }
 );
