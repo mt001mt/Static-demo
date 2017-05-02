c45a16ccaf0c63cae83e69e46d5993fab802d255
MT <mt001mt@163.com>
2017-05-02 15:37:31 +0800

    before delete row--1 class

--- scripts/index-imitation of Tenda.js
+++ scripts/index-imitation of Tenda.js
@@ -1,5 +1,13 @@
 // JavaScript Document
-
+window.addEventListener("load", function () {
+  scrollTo(0, 5000);
+  /*setTimeout(function () {
+    scrollTo(0, 1000);
+  }, 300);
+  setTimeout(function () {
+    scrollTo(0, 1500);
+  }, 600);*/
+});
 $(function () {
   var hovershowDuration = 200, //hover时显示下拉菜单的时间，单位ms
       temp = $(".user-hovershow-toggle"),
@@ -7,7 +15,10 @@ $(function () {
       hovershowB = temp[1], //.
       hovershowedA = $("#hovershow-1")[0], //hover时显示的下拉菜单
       hovershowedB = $("#hovershow-2")[0], //.
-      carouselControl = $(".carousel-control"); //轮播图片的翻页按钮
+      $carouselControl = $(".carousel-control"), //轮播图片的翻页按钮
+      $lazyArray = $(".lazy"), //需要延迟加载的图片
+      TopButtonVisibility = false, //返回顶部按钮是否可见
+      xs = $(window).width() < 768 ? true : false; //现在是不是xs
       
   temp = null;
   
@@ -19,36 +30,64 @@ $(function () {
   //关闭transition效果
   //$.support.transition = false;
   //设置自定义的图片加载技术
-  $(".user-hovershow-toggle").one("mouseover", getLazyImage);
+  $(".user-hovershow-toggle").one("mouseover", getGroupLazyImage);
   //按需求重新添加下拉菜单功能
   $(".dropdown-toggle").dropdown();
   $(document).on("click", ".user-dropdown-toggle", function () {
     $(this).parent().toggleClass("open");
     return false;
   });
-  //添加mouseover事件，显示下拉菜单
-  $(document).on("mouseover", ".user-hovershow-toggle", hoverSlideDown);
-  //添加mouseout事件，隐藏下拉菜单
-  $(document).on("mouseout", ".user-hovershow-toggle", hoverSlideUp);
-  $(document).on("mouseout", ".hovershow", hoverSlideUp);
+  //添加导航栏显示、隐藏下拉菜单事件
+  $(".user-hovershow-toggle").hover(hoverSlideDown, hoverSlideUp);
+  $(".hovershow").on("mouseout", hoverSlideUp);
   //添加应该在开始就加载的图片
-  getLazyImage(".lazy-bg-onload", 768, " @2x");
+  getGroupLazyImage(".lazy-onload", 768, " @2x");
   //给轮播图片的翻页按钮添加显隐动画
   $("#carousel").hover(function () {
-    carouselControl.stop(true).fadeIn(300);
+    $carouselControl.stop(true).fadeIn(300);
   }, function () {
-    carouselControl.fadeOut(300);
+    $carouselControl.fadeOut(300);
   });
-
+  //给轮播图片设置window的resize事件，当$(window).width()在768上下浮动时，
+  //轮播图片需要在@2x和非@2x之间浮动
+  $(window).on("resize", function () {
+    var viewWidth = $(window).width();
+    if (viewWidth >= 768 && xs) {
+      getGroupLazyImage("#carousel .lazy-onload");
+      xs = false;
+    } else if (viewWidth < 768 && !xs) {
+      getGroupLazyImage(".lazy-onload", 768, " @2x");
+      xs = true;
+    }
+  });
+  //设置图片延迟加载事件
+  lazyImagehandler();
+  //给返回顶部按钮添加click事件和scroll显隐事件
+  $("#top").on("click", function () {
+    scrollTo(0, 0);
+  });
+  $(window).on("scroll", function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop >= 200 && !TopButtonVisibility) {
+      $("#top").stop(true).fadeIn("slow");
+      TopButtonVisibility = true;
+    } else if (scrollTop < 200 && TopButtonVisibility) {
+      $("#top").fadeOut("slow");
+      TopButtonVisibility = false;
+    }
+  });
+  //给返回顶部按钮添加click事件和scroll显隐事件
+  
   /************************以下是函数定义********************************/
   /*******************************************************************/
-  //实现图片延迟加载，根据参数找到img的容器，
-  //再根据容器的data-lazy，按顺序给img的src赋值、
+  //实现图片 组 的延迟加载，根据参数找到img的容器，
+  //再根据容器的data-original，按顺序给img的src赋值、
   //@param1 selector 可选类型为Event、String，根据该值找到img的容器
   //@param2 width Num 可无，默认无。若有，表示当document.offsetWidth < width时，src加后缀
   //@param3 sufsrc String 可无，默认无。若有，表示当document.offsetWidth < width时，src加后缀。须和width同用。
+  //@return undefined
   //可直接调用，可被用作事件处理程序
-  function getLazyImage (selector, width, sufsrc) {
+  function getGroupLazyImage (selector, width, sufsrc) {
     var $parent, $img, src, suf, lastIndex;
     
     if (typeof selector === "string") {
@@ -62,9 +101,9 @@ $(function () {
       return;
     }
     
-    src = $parent.attr("data-lazy");
-    
-    if (width && document.documentElement.offsetWidth < width) {
+    src = $parent.attr("data-original");
+
+    if (width && document.documentElement.clientWidth < width) {
       lastIndex = src.lastIndexOf(".");
       suf = src.substring(lastIndex);
       src = src.substring(0, lastIndex);
@@ -82,8 +121,76 @@ $(function () {
     });
     
   }
+  //实现图片的延迟加载，包括添加resize、scoll、DOMContentLoaded事件，
+  //判断需要加载的图片，加载图片等一系列操作
+  //@param None
+  //@return undefined
+  function lazyImagehandler () {
+    $(window).on("resize scroll", getImage);
+    $(document).ready(getImage);
+    //遍历需要延迟加载图片的元素，并按需求加载图片
+    //图片加载后，将该元素从加载图片队列中去除
+    function getImage () {
+      var $delete = $(); //需要删除的$队列
+      $lazyArray.each(function () {
+        var $this = $(this),
+            mask = isInView($this);
+        if (mask === 6) {
+          return false; //被检测的元素在视口之右下，就不检测了后面的元素了。不鲁棒
+        } else if (mask === 0) {
+          $this.attr("src", $this.attr("data-original"));
+          $delete = $delete.add($this);
+        }
+      });
+      $lazyArray = $lazyArray.not($delete);
+      
+      if ($lazyArray.length === 0) {
+        $(window).off("resize scroll", getImage);
+      }
+    }
+    //判断元素是否在可视范围内
+    //@param1 $Element 需要是否在可是范围内的元素
+    //@return Number 全部位于视口上边 1
+    //                         右边 2
+    //                         下边 4
+    //                         左边 8
+    //                         里面 0
+    //PS：js的位运算奇葩得比较慢，采用非位运算
+    function isInView ($element) {
+      var offsetTop = $element.offset().top,
+          offsetLeft = $element.offset().left,
+          elementHeight = $element.height(),
+          elementWidth = $element.width(),
+          $window = $(window),
+          height = $window.height(),
+          width = $window.width(),
+          scrollTop = $window.scrollTop(),
+          scrollLeft = $window.scrollLeft(),
+          mask = 0;
+      //位于上边
+      if (offsetTop + elementHeight < scrollTop) {
+        mask += 1;
+        //位于左边
+        if (offsetLeft + elementWidth < scrollLeft) {
+          mask += 8;
+        }
+        //位于右边
+        else if (offsetLeft > scrollLeft + width) {
+          mask += 2;
+        }
+      }
+      //位于下边
+      else if (offsetTop > scrollTop + height) {
+        mask += 4;
+      }
+      
+      return mask;
+    }
+    
+  }
+  //实现图片的延迟加载
   //实现背景图的延迟加载，将参数对应的元素的data-target找到img的容器，
-  //再根据容器的data-lazy，按顺序给img的src赋值
+  //再根据容器的data-original，按顺序给img的src赋值
   //
   function getLazyBackgroundImage (selector) {
     if (!selector) {
@@ -91,7 +198,7 @@ $(function () {
     }
     var $parent = $(selector),
         $img = $parent.find("img"),
-        src = $parent.attr("data-lazy-bg"),
+        src = $parent.attr("data-original-bg"),
         width = document.offsetWidth,
         lastIndex, presrc, sufsrc;
     
@@ -153,5 +260,4 @@ $(function () {
       return;
     }
   }
-
 });
